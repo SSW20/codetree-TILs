e901296db104d7226fd58ef624cbfb4d13c8b4ab
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-07-22 11:23:00 +0900

    Title: 1부터 특정 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
+++ 240722/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,total =0;
+    int n,total=0;
     cin >> n;
     for(int i=1; i<=n; ++i) total += i;
-    cout << total / 10'
+    cout << total / 10;
     return 0;
 }
