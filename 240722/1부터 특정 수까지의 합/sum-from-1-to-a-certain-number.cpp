6da033da463311c51ecb748aae621c3a997a9271
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-07-22 11:22:37 +0900

    Title: 1부터 특정 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240722/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,total =0;
+    cin >> n;
+    for(int i=1; i<=n; ++i) total += i;
+    cout << total / 10'
+    return 0;
+}
