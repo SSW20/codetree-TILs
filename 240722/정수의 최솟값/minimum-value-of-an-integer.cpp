dfc50c668ee325ba547a43b04c82b5e914027df2
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-07-22 11:27:32 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin >> a >> b >> c;
+    int min = a;
+    if(a > c && b > c){ min = c;}
+    if(a > b && c > b){ min = b;}
+    cout << min;
+    return 0;
+}
