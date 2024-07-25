877985dc5236afc48f74ece319f3fbea3fae7266
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-07-25 11:41:47 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240725/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 240725/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -11,12 +11,14 @@ bool isValid(int *count, string s){
             ++count[c-'a'];
         }
     }
+    return false;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     string s;
-    int arr[26] = {0,};
+    int arr[26];
+    for(int i=0;i<26;++i) arr[i] = 0;
     cin >> s;
     if(isValid(arr, s)) cout << "Yes";
     else cout << "No";
