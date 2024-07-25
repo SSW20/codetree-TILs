d12fb75cfb7f8328709d31f0151047a866a30526
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-07-25 22:12:27 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printAsterisk(int n){
+    if(n == 0) return;
+    for(int i=0; i<n;++i){
+        cout << "* ";
+    }
+    cout << '\n';
+    printAsterisk(n-1);
+    for(int i=0; i<n;++i){
+        cout << "* ";
+    }
+    cout << '\n';
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    printAsterisk(n);
+    return 0;
+}
