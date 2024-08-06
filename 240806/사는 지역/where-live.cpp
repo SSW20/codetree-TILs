79411b8ff392a4543980813a02f55643f7018308
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-08-06 21:41:59 +0900

    Title: 사는 지역, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/사는 지역/where-live.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Person{
+    public:
+        string name;
+        string addr;
+        string city;
+        Person(string n,string a, string c){
+            this->name  = n;
+            this->addr = a;
+            this->city = c;
+        }
+};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string name;
+    string addr;
+    string city;
+
+    vector<Person*> v;
+    int n;
+    cin>> n;
+    for(int i=0;i<n;++i){
+        cin >> name >> addr >> city;
+        v.push_back(new Person(name,addr,city));
+    }
+
+    sort(v.begin(),v.end());
+    cout << "name " << v[0]->name << '\n';
+    cout << "addr " << v[0]->addr << '\n';
+    cout << "city " << v[0]->city << '\n';
+    
+
+    return 0;
+}
