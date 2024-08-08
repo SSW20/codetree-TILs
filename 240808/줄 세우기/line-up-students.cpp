06eb72cb036dfec316c3cd1f42faa5ce7a19f0a6
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-08-08 13:23:41 +0900

    Title: 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/줄 세우기/line-up-students.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+class Student {
+public:
+    int idx,h,w;
+    Student(int idx, int h, int w) {
+        this->idx = idx;
+        this->h = h;
+        this->w = w;
+    }
+};
+
+bool comp(const Student& a, const Student& b) {
+    if (a.h == b.h) {
+        if (a.w == b.w) return a.idx < b.idx;
+        else a.w > b.w;
+    }
+    else return a.h > b.h;
+}
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+    vector<Student> v;
+
+    int w;
+    int h;
+    for (int i = 0; i < n; ++i) {
+        cin >> h >> w;
+        v.push_back(Student(i + 1, h, w));
+   }
+
+
+    sort(v.begin(), v.end(), comp);
+
+    for (int i = 0; i < n; ++i) {
+        cout  << v[i].h << ' ' << v[i].w << ' ' << v[i].idx << '\n';
+    }
+
+    return 0;
+}
