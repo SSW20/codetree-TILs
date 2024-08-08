b16ac9029651af56d4157d715712de30959ef40d
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-08-08 15:51:51 +0900

    Title: Date to Date, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/Date to Date/date-to-date.cpp
+++ 240808/Date to Date/date-to-date.cpp
@@ -9,10 +9,12 @@ int main() {
     int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int total = num_of_days[m1] - d1;
+    int total = 0;
 
-    for(int i=m1 + 1; i<m2; ++i){ total += num_of_days[i];}
-    total += d2;
+    
+    for(int i=m1; i<m2; ++i){ total += num_of_days[i];}
+    if(m1 == m2) total += d2 - d1;
+    else total += d2 - d1;
     cout << total + 1;
     return 0;
 }
