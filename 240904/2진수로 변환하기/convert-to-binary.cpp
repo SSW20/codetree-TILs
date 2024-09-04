2ee567621a4ae29e9905bebb4ea4ed72d6bae1db
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-04 21:42:50 +0900

    Title: 2진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/2진수로 변환하기/convert-to-binary.cpp
+++ 240904/2진수로 변환하기/convert-to-binary.cpp
@@ -6,10 +6,13 @@ int main() {
 	int n;
 	stack<int> st;
 	cin >> n;
+
+	if (n == 1 || n == 0) { cout << n; return 0; }
 	while (n != 0) {
 		st.push(n % 2);
 		n = n / 2;
 	}
+
 
 	while (!st.empty()) {
 		cout << st.top();
