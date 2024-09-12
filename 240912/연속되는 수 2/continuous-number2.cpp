a4807eb88662c3835a8e0d7c0baa432601298d86
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-12 17:16:56 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240912/연속되는 수 2/continuous-number2.cpp
+++ 240912/연속되는 수 2/continuous-number2.cpp
@@ -13,13 +13,13 @@ int main() {
 	cur = x;
 	n--;
 	
+	if (n == 0) { cout << 1; return 0; }
+
 	while (n-- > 0) {
 		cin >> x;
 		if (cur == x) ++cnt;
 		else { if (max < cnt) { max = cnt; cnt = 1; } }
 		cur = x;
 	}
-
-	if (max < cnt) max = cnt;
 	cout << max;
 }
