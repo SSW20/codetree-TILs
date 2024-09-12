26f8ccaf74e085c98dc891d5918e2927d8eaa8b4
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-12 17:26:10 +0900

    Title: 연속되는 수 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	for (int i = 0; i < n; ++i) cin >> v[i];
+
+	int cnt = 1;
+	int max = 1;
+	for (int i = 0; i < n-1; ++i) {
+		if (v[i] < v[i+1]) ++cnt;
+		else {
+			cnt = 1;
+		}
+		if (max < cnt) max = cnt;
+ 	}
+	
+	cout << max;
+	return 0;
+}
