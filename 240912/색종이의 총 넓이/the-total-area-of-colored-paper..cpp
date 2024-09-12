ae150c9a4ac99a55ae026dd7d1b87f31b4242a1d
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-12 16:28:59 +0900

    Title: 색종이의 총 넓이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240912/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
+++ 240912/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
@@ -6,10 +6,13 @@ int main() {
 	int N;
 	cin >> N;
 
-	vector<vector<int>> v(101, vector<int>(101));
+	vector<vector<int>> v(201, vector<int>(201));
 	while (N-- > 0) {
 		int x, y;
 		cin >> x >> y;
+
+		x += 100;
+		y += 100;
 
 		for (int i = y; i < y + 8; ++i) {
 			for (int k = x; k < x + 8; ++k) {
@@ -19,8 +22,8 @@ int main() {
 	}
 
 	int cnt = 0;
-	for (int i = 0; i < 101; ++i) {
-		for (int k = 0; k < 101; ++k) {
+	for (int i = 0; i < 201; ++i) {
+		for (int k = 0; k < 201; ++k) {
 			if (v[i][k] == 1) ++cnt;
 		}
 	}
