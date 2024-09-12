e518acd7a66efd42020ce3415fc920f4cdc37a79
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-12 17:48:21 +0900

    Title: 만나는 그 순간, Time: 10ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240912/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n,m;
+	cin >> n >> m;
+	vector<int> v(1000001,0);
+	vector<int> v2(1000001,0);
+
+	char c;
+	int t;
+
+
+	int idx = 1;
+	while (n-- > 0) {
+		cin >> c >> t;
+		while (t-- > 0) {
+			if (c == 'R') {
+				v[idx] = v[idx - 1]+  1;
+			}
+			else {
+				v[idx] = v[idx - 1]- 1;
+			}
+			idx++;
+		}
+	}
+	idx = 1;
+	while (m-- > 0) {
+		cin >> c >> t;
+		while (t-- > 0) {
+			if (c == 'R') {
+				v2[idx] = v2[idx-1]+ 1;
+			}
+			else {
+				v2[idx] = v2[idx-1]- 1;
+			}
+			idx++;
+		}
+	}
+
+	for (int i = 1; i < 1000001; ++i) {
+		if (v[i] == v2[i]) { cout << i; break; }
+	}
+	return 0;
+}
