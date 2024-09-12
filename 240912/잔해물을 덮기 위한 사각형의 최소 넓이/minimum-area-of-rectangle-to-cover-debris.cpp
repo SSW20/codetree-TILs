dc9bd26a53ab6e1fe69a5d2f48bb9a2ab1d73a76
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-12 16:41:22 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 19ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240912/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	vector<vector<int>> v(2001, vector<int>(2001));
+	int N = 2;
+	int flag = true;
+	while (N-- > 0) {
+		int x, y, x1, y1;
+		cin >> x >> y >> x1 >> y1;
+
+		x += 1000;
+		y += 1000;
+		y1 += 1000;
+		x1 += 1000;
+
+		for (int i = y; i < y1; ++i) {
+			for (int k = x; k < x1; ++k) {
+				if (flag) v[i][k] = 1;
+				else v[i][k] = 0;
+			}
+		}
+
+		flag = false;
+	}
+
+	int maxX = -1, maxY = -1, minX = 10000, minY = 10000;
+	for (int i = 0; i < 2001; ++i) {
+		for (int k = 0; k < 2001; ++k) {
+			{
+				if (v[i][k] == 1) {
+					if (maxX <= k && maxY <= i) {
+						maxX = k;
+						maxY = i;
+					}
+					if (minX >= k && minY >= i) {
+						minX = k;
+						minY = i;
+					}
+				}
+
+			}
+		}
+	}
+
+	cout << (maxX - minX + 1) * (maxY - minY + 1);
+
+	return 0;
+}
