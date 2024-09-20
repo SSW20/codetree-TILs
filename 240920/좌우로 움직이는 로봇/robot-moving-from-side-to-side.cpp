186d31e21719cdf25846ed8fcda0d55223837c9b
SSW20 <76836826+SSW20@users.noreply.github.com>
2024-09-20 09:31:47 +0900

    Title: 좌우로 움직이는 로봇, Time: 10ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240920/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	char c;
+	vector<int> a(1000001,0);
+	vector<int> b(1000001,0);
+	cin >> n >> m;
+	int idx = 1;
+	while (n-- > 0) {
+		cin >> t >> c;
+		while (t-- > 0) {
+			if (c == 'L') {
+				a[idx] = a[idx - 1] -1;
+			}
+			else {
+				a[idx] = a[idx - 1] + 1;
+			}
+			idx++;
+		}
+	}
+
+	idx = 1;
+	while (m-- > 0) {
+		cin >> t >> c;
+		while (t-- > 0) {
+			if (c == 'L') {
+				b[idx] = b[idx - 1] - 1;
+			}
+			else {
+				b[idx] = b[idx - 1] + 1;
+			}
+			idx++;
+		}
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < 1000000; ++i) {
+		if (a[i] != b[i] && a[i + 1] == b[i + 1]) ++cnt;
+	}
+
+	std::cout << cnt;
+	return 0;
+}
