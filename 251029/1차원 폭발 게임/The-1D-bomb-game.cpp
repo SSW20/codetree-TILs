22718aeed637149e364bde8658fa35b435733d13
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-10-29 17:56:22 +0900

    Title: 1차원 폭발 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251029/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 251029/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -1,80 +1,81 @@
-#include <iostream>
-#include <stack>
-using namespace std;
+    #include <iostream>
+    #include <stack>
+    using namespace std;
 
-int n, m;
-int numbers[100];
+    int n, m;
+    int numbers[100];
 
-int main() {
-    cin >> n >> m;
-    stack<int> st;
-    for (int i = 0; i < n; i++) {
-        cin >> numbers[i];
-    }
-
-    int startIdx = 0;
-    int count = 0;
-    int flag = numbers[startIdx];
-    int endIdx = 0;
-    while (endIdx < n)
-    {
-        if (numbers[endIdx] == 0)
-        {
-            ++endIdx;
-            continue;
+    int main() {
+        cin >> n >> m;
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            cin >> numbers[i];
         }
-        if (flag == numbers[endIdx])
+
+        int startIdx = n-1;
+        int count = 0;
+        int flag = -1;
+        int endIdx = n-1;
+        while (endIdx >= 0)
         {
-            ++count;
-            ++endIdx;
-        }
-        else
-        {
-            if (count >= m)
+            if (numbers[endIdx] == 0)
             {
-                for (int i = startIdx; i < endIdx; ++i)
-                {
-                    numbers[i] = 0;
-                }
-                startIdx = 0;
-                flag = numbers[startIdx];
-                endIdx = 0;
-                count = 0;
+                --endIdx;
+                continue;
+            }
+            if (flag == numbers[endIdx])
+            {
+                ++count;
+                --endIdx;
             }
             else
             {
-                startIdx = endIdx;
-                flag = numbers[startIdx];
-                ++endIdx;
+                if (count >= m)
+                {
+                    for (int i = startIdx; i > endIdx; --i)
+                    {
+                        numbers[i] = 0;
+                    }
+                    startIdx = n-1;
+                    flag = numbers[startIdx];
+                    endIdx = n-1;
+                    count = 0;
+                }
+                else
+                {
+                    startIdx = endIdx;
+                    flag = numbers[startIdx];
+                    --endIdx;
+                    count = 1;
+                }
             }
         }
-    }
 
-    if (count >= m)
-    {
-        for (int i = startIdx; i < endIdx; ++i)
+        if (count >= m)
         {
-            numbers[i] = 0;
+            for (int i = startIdx; i > endIdx; --i)
+            {
+                numbers[i] = 0;
+            }
         }
-    }
 
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (numbers[i] > 0)
+        int ans = 0;
+        for (int i = 0; i < n; i++)
         {
-            ++ans;
+            if (numbers[i] > 0)
+            {
+                ++ans;
+            }
         }
-    }
-    cout << ans << '\n';
-    for (int i = 0; i < n; i++)
-    {
-        if (numbers[i] > 0)
+        cout << ans << '\n';
+        for (int i = 0; i < n; i++)
         {
-            cout << numbers[i] << '\n';
+            if (numbers[i] > 0)
+            {
+                cout << numbers[i] << '\n';
+            }
         }
-    }
-    // Please write your code here.
+        // Please write your code here.
 
-    return 0;
-}
+        return 0;
+    }
