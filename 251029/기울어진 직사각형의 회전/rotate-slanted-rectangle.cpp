05ed7e3a5a0d3d78183c854e5670c0a74eedab46
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-10-29 12:36:52 +0900

    Title: 기울어진 직사각형의 회전, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251029/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
+++ 251029/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
@@ -1,74 +1,10 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int n;
 int grid[100][100];
 int r, c, m1, m2, m3, m4, dir;
-
-
-void circle(int r, int c,int m1, int m2, int m3, int m4)
-{
-    int temp = grid[r - m1][c + m1];
-    // 오른쪽 위 m1
-
-    for (int i = m1; i > 0; --i)
-    {
-        grid[r - i][c + i] = grid[r  - i + 1][c + i - 1];
-    }
-    // 왼쪽 아래 m4
-    for (int i = 0; i < m4; ++i)
-    {
-        grid[r + i][c + i] = grid[r + i - 1][c + i - 1];
-    }
-    // 왼쪽 아래 m3
-    int x = r - m4;
-    int y = c - m4;
-    for (int i = 0; i < m3; ++i)
-    {
-        grid[x -i][y + i] = grid[x -i - 1][y + i + 1];
-    }
-    // 왼쪽 위 m2
-    x = r - m1;
-    y = c + m1;
-    for (int i = m1; i > 1; --i)
-    {
-        grid[x - i][y - i] = grid[x - i + 1][y - i + 1];
-    }   
-    grid[x-1][y-1] = temp;
-}
-void reverse(int r, int c, int m1, int m2, int m3, int m4)
-{
-    int temp = grid[r - m4][c - m4];
-    // m4
-    for (int i = m4; i > 0; --i)
-    {
-        grid[r - i][c - i] = grid[r - i + 1][c - i + 1];
-    }
-    //m1
-    for (int i = m1; i > 0; --i)
-    {
-        grid[r - (m1-i)][c + (m1 - i)] = grid[r -(m1 - i) - 1][c + (m1 - i) + 1];
-    }
-    // m2
-    int x = r - m1;
-    int y = c + m1;
-    for (int i = 0; i < m2; ++i)
-    {
-        grid[x - i][y - i] = grid[x - i - 1][y - i - 1];
-    }
-    // m3
-    x -= m2;
-    y -= m2;
-    for (int i = 0; i < m3 - 1; ++i)
-    {
-        grid[x + i][y - i] = grid[x + i + 1][y - i - 1];
-    }
-
-    grid[x + m3 - 1][y - m3 + 1] = temp;
-   
-}
-
 int main() {
     cin >> n;
 
@@ -76,23 +12,80 @@ int main() {
         for (int j = 0; j < n; j++) cin >> grid[i][j];
 
     cin >> r >> c >> m1 >> m2 >> m3 >> m4 >> dir;
-
-    // Please write your code here.
+    vector<int> v;
+    int x = r - 1;
+    int y = c - 1;
+    for (int i = 0; i < m1; ++i)
+    {
+        v.push_back(grid[x][y]);
+        x--; y++;
+    }
+ 
+    for (int i = 0; i < m2; ++i)
+    {
+        v.push_back(grid[x][y]);
+        x--; y--;
+    }
+  
+    for (int i = 0; i < m3; ++i)
+    {
+        v.push_back(grid[x][y]);
+        x++; y--;
+    }
+  
+    for (int i = 0; i < m4; ++i)
+    {
+        v.push_back(grid[x][y]);
+        x++; y++;
+    }
     if (dir == 0)
     {
-        circle(r-1,c-1,m1, m2, m3, m4);
+        // 오른쪽 shift
+        int temp = v[v.size() - 1];
+        for (int i = v.size() - 1; i > 0; --i)
+        {
+            v[i] = v[i - 1];
+        }
+        v[0] = temp;
     }
     else
     {
-        reverse(r-1,c-1,m1, m2, m3, m4);
+        int temp = v[0];
+        for (int i = 0; i < v.size() - 1; ++i)
+        {
+            v[i] = v[i + 1];
+        }
+        v[v.size()-1] = temp;
     }
+    x = r - 1;
+    y = c - 1;
 
+    int idx = 0;
+    for (int i = 0; i < m1; ++i)
+    {
+        grid[x][y] = v[idx];
+        x--; y++; ++idx;
+    }
+    for (int i = 0; i < m2; ++i)
+    {
+        grid[x][y] = v[idx];
+        x--; y--; ++idx;
+    }
+  
+    for (int i = 0; i < m3; ++i)
+    {
+        grid[x][y] = v[idx];
+        x++; y--; ++idx;
+    }
+   
+    for (int i = 0; i < m4; ++i)
+    {
+        grid[x][y] = v[idx];
+        x++; y++; ++idx;
+    }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            cout << grid[i][j] << ' ';
-        }
+        for (int j = 0; j < n; j++) cout << grid[i][j] << ' ';
         cout << '\n';
     }
     return 0;
