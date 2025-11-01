18db3fe2fe2000730d9d03715cec914cb01a523f
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-01 15:07:37 +0900

    Title: 주사위 던지기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251101/주사위 던지기/roll-a-dice.cpp
+++ 251101/주사위 던지기/roll-a-dice.cpp
@@ -4,75 +4,48 @@ using namespace std;
 
 int n, m;
 int r, c;
+char dir;
 int map[101][101];
-int dice[4][4] = { {0,1,0,0},{0,5,0,0},{4,6,3,1},{0,2,0,0} };
-void shift(int &r, int &c, char dir)
-{
-    //가로 2 / 세로 1
-    if (dir == 'R')
-    {
-        if (c + 1 > n) return;
-        //가로 왼쪽 당김
-        int temp = dice[2][0];
-        for (int i = 0; i < 3; ++i)
-        {
-            dice[2][i] = dice[2][i + 1];
-        }
-        dice[2][3] = temp;
-        map[r][++c] = dice[2][1];
-    }
-    else if (dir == 'D')
-    {
-        if (r + 1 > n) return;
-        //세로 위쪽 당김
-        int temp = dice[0][1];
-        for (int i = 0; i < 3; ++i)
-        {
-            dice[i][1] = dice[i+1][1];
-        }
-        dice[3][1] = temp;
-        map[++r][c] = dice[2][1];
-    }
-    else if (dir == 'L')
-    {
-        if (c - 1 < 1) return;
-
-        //가로 오른쪽 당김
-        int temp = dice[2][3];
-        for (int i = 3; i > 0; --i)
-        {
-            dice[2][i] = dice[2][i - 1];
-        }
-        dice[2][0] = temp;
-        map[r][--c] = dice[2][1];
-
-    }
-    else
-    {
-        if (r - 1 < 0) return;
-
-        //세로 아래 당김
-        int temp = dice[3][1];
-        for (int i = 3; i > 0; --i)
-        {
-            dice[i][1] = dice[i - 1][1];
-        }
-        dice[0][1] = temp;
-        map[--r][c] = dice[2][1];
-
-    }
-    return;
-}
 int main() {
     cin >> n >> m >> r >> c;
-    char dir;
+    int front = 2;
+    int right = 3;
+    int up = 1;
     map[r][c] = 6;
-
-    int idX = 1;
-    int idY = 2;
     for (int i = 0; i < m; i++) {
         cin >> dir;
-        shift(r, c, dir);
+        if (dir == 'R')
+        {
+            if (c + 1 > n) continue;
+            map[r][++c] = right;
+            int temp = right;
+            right = up;
+            up = 7 - temp;
+        }
+        else if (dir == 'L')
+        {
+            if (c - 1 < 1) continue;
+            map[r][--c] = 7-right;
+            int temp = right;
+            right = 7 - up;
+            up = temp;
+        }
+        else if (dir == 'U')
+        {
+            if (r - 1 < 1) continue;
+            map[--r][c] = 7 - front;
+            int temp = front;
+            front = 7 - up;
+            up = temp;
+        }
+        else
+        {
+            if (r + 1 > n) continue;
+            map[++r][c] = front;
+            int temp = front;
+            front = up;
+            up = 7 - temp;
+        }
     }
     int ans = 0;
     for (int i = 0; i < n; ++i)
@@ -82,9 +55,7 @@ int main() {
             ans += map[i][k];
         }
     }
-
-    cout << ans;
     // Please write your code here.
-
+    cout << ans;
     return 0;
 }
