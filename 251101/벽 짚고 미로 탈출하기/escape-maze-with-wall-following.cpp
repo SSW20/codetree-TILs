6b7ef0d8d4003da0c3acd3d17049dd5e4d1221a5
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-01 13:29:11 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 1092ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 251101/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
+++ 251101/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -9,10 +9,6 @@ char grid[101][101];
 // 오 하 좌 상
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
-
-
-
-
 int main() {
     cin >> N;
     cin >> x >> y;
@@ -22,7 +18,7 @@ int main() {
             cin >> grid[i][j];
         }
     }
-    int count = 1;
+    int  count = 0;
     int d = 0;
     int nextX = x;
     int nextY = y;
@@ -30,20 +26,17 @@ int main() {
         cout << -1;
         return 0;
     }
+    grid[x][y] = 1;
     while (count <= N * N)
     {
-        nextX += dx[d];
-        nextY += dy[d];
-
-        if (grid[nextX][nextY] == '.') ++count;
         if (nextX <= 0 || nextY <= 0 || nextX > N || nextY > N) break;
         // 한칸 이동 시  벽이 있을 때  
-        if (grid[nextX][nextY] == '#')
+        while (grid[nextX + dx[d]][nextY + dy[d]] == '#')
         {
             d = (d + 3) % 4;
         }
         // 없을 때
-        else
+        if (grid[nextX + dx[d]][nextY + dy[d]] != '#')
         {
             // 벽있을때
             if (d == 0)
@@ -56,7 +49,7 @@ int main() {
                 if (grid[nextX][nextY - 1] != '#')
                     d = (d + 1) % 4;
             }
-            else if(d == 2)
+            else if (d == 2)
             {
                 if (grid[nextX - 1][nextY] != '#')
                     d = (d + 1) % 4;
@@ -67,16 +60,21 @@ int main() {
                     d = (d + 1) % 4;
             }
         }
+
+        nextX += dx[d];
+        nextY += dy[d];
+        ++count;
+
     }
     // Please write your code here.
     if (count > N * N)
     {
         cout << -1;
     }
-    else 
-    {
+    else {
         cout << count;
     }
+    
 
     return 0;
 }
