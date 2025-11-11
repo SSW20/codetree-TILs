36fd38b63fb303e486b97c41bea8560ee1c0dace
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-11 11:18:26 +0900

    Title: 합쳐지는 구슬들, Time: 9ms, Memory: 2MB, Status: Passed - Codetree

--- 251111/합쳐지는 구슬들/merge-marbles.cpp
+++ 251111/합쳐지는 구슬들/merge-marbles.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
 int N, M, T;
-int r, c,  w;
+int r, c, w;
 char d;
 int asci[128];
 
 
 // r c w 번호 d
-using Tuple = tuple<int, int, int,int, int>;
+using Tuple = tuple<int, int, int, int, int>;
 
 // w 번호  d
-vector<tuple<int, int, int>> grid[50][50];
+queue<tuple<int, int, int>> grid[50][50];
 int main()
 {
 	cin >> N >> M >> T;
@@ -26,11 +27,11 @@ int main()
 
 	vector<Tuple> numbers(M);
 
-	for(int i=0; i<M;++i)
+	for (int i = 0; i < M; ++i)
 	{
 		cin >> r >> c >> d >> w;
-		numbers[i] = { r - 1,c - 1,w,i+1,asci[d] };
-		grid[r - 1][c - 1].push_back({ w,i + 1,asci[d] });
+		numbers[i] = { r - 1,c - 1,w,i + 1,asci[d] };
+		grid[r - 1][c - 1].push({ w,i + 1,asci[d] });
 	}
 
 	while (T-- > 0)
@@ -41,28 +42,13 @@ int main()
 			tie(x, y, w, num, d) = numbers[i];
 			int dx[4] = { -1,0,1,0 };
 			int dy[4] = { 0,1,0,-1 };
-			grid[x][y].pop_back();
+			grid[x][y].pop();
 
 			if (x + dx[d] < 0 || x + dx[d] >= N || y + dy[d] < 0 || y + dy[d] >= N) { d = (d + 2) % 4; }
-			else {x += dx[d]; y += dy[d];}
-
-			
-			if(grid[x][y].empty()) { grid[x][y].push_back({ w,num,d });}
-			else
-			{
-				int preW, preNum, preD;
-				tie(preW, preNum, preD) = grid[x][y].front();
-
-				if (num < preNum)
-				{
-					num = preNum;
-					d = preD;
-				}
-
-				grid[x][y].pop_back();
-				grid[x][y].push_back({ w + preW, num, d });
-			}
+			else { x += dx[d]; y += dy[d]; }
+			grid[x][y].push({ w,num,d });
 		}
+
 		numbers.clear();
 		for (int i = 0; i < N; ++i)
 		{
@@ -72,22 +58,41 @@ int main()
 				{
 					int w, d, num;
 					tie(w, num, d) = grid[i][k].front();
-					numbers.push_back({ i,k,w,num,d });
+
+					if (grid[i][k].size() == 1) numbers.push_back({ i,k,w,num,d });
+					else
+					{
+						// w 번호 d
+						grid[i][k].pop();
+						while (!grid[i][k].empty())
+						{
+							int curW, curD, curNum;
+							tie(curW, curNum, curD) = grid[i][k].front();
+
+							if (curNum > num)
+							{
+								num = curNum;
+								d = curD;
+							}
+							w += curW;
+							grid[i][k].pop();
+						}
+						numbers.push_back({ i,k,w,num,d });
+						grid[i][k].push({ w,num,d });
+					}
 				}
 			}
 		}
-
 	}
+		int count = numbers.size();
+		int maxWeight = -1;
 
-	int count = numbers.size();
-	int maxWeight = -1;
+		for (int i = 0; i < numbers.size(); ++i)
+		{
+			int x, y, w, d, num;
+			tie(x, y, w, num, d) = numbers[i];
+			if (maxWeight < w) maxWeight = w;
+		}
 
-	for (int i = 0; i < numbers.size(); ++i)
-	{
-		int x, y, w, d, num;
-		tie(x, y, w, num, d) = numbers[i];
-		if (maxWeight < w) maxWeight = w;
+		cout << count << ' ' << maxWeight;
 	}
-
-	cout << count << ' ' << maxWeight;
-}
