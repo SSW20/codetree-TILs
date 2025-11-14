6672a2f6dc24a9a81ef8f8a4a14b215de83cc740
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-14 10:47:24 +0900

    Title: 알파벳과 사칙연산, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251114/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+string expr;
+int ansMax = 4;
+vector<int> alpha(4);
+
+void Calc(int idx, int total)
+{
+    if (idx >= expr.size())
+    {
+        ansMax = max(ansMax, total);
+        return;
+    }
+
+    char sign = expr[idx];
+    char alp = expr[idx + 1];
+
+    if (sign == '+')
+    {
+        if (alpha[alp - 'a'] == 0)
+        {
+            for (int i = 1; i <= 4; ++i)
+            {
+                alpha[alp - 'a'] = i;
+                Calc(idx + 2, total + i);
+            }
+        }
+        else
+        {
+            Calc(idx + 2, total + alpha[alp - 'a']);
+        }
+    }
+    else if (sign == '-')
+    {
+        if (alpha[alp - 'a'] == 0)
+        {
+            for (int i = 1; i <= 4; ++i)
+            {
+                alpha[alp - 'a'] = i;
+                Calc(idx + 2, total -i);
+            }
+        }
+        else
+        {
+            Calc(idx + 2, total - alpha[alp - 'a']);
+        }
+    }
+    else
+    {
+        if (alpha[alp - 'a'] == 0)
+        {
+            for (int i = 1; i <= 4; ++i)
+            {
+                alpha[alp - 'a'] = i;
+                Calc(idx + 2, total * i);
+            }
+        }
+        else
+        {
+            Calc(idx + 2, total * alpha[alp - 'a']);
+        }
+    }
+}
+int main() {
+    cin >> expr;
+
+    // Please write your code here.
+    alpha[expr[0] - 'a'] = 4;
+
+
+    Calc(1, 4);
+    cout << ansMax;
+
+    return 0;
+}
