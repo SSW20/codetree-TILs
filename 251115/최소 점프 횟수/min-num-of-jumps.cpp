2dc8b20d6d88793e301331e73ae95624f56ffbb9
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-15 14:11:12 +0900

    Title: 최소 점프 횟수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251115/최소 점프 횟수/min-num-of-jumps.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n;
+int jump[10];
+int ansMin = 200;
+void Select(int cnt, int curIdx)
+{
+    if (cnt == n)
+    {
+        return;
+    }
+    if (curIdx == n)
+    {
+        ansMin = min(ansMin, cnt);
+        return;
+    }
+
+    for (int i = 0; i <= jump[cnt]; ++i)
+    {
+        Select(cnt + 1, curIdx + i);
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> jump[i];
+    }
+    Select(0, 0);
+    if (ansMin == 200) cout << -1;
+    else cout << ansMin;    
+    // Please write your code here.
+
+    return 0;
+}
