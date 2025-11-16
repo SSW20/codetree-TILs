f505d334d6dad328f00b8982b70fa03ea12ad91b
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-16 11:35:55 +0900

    Title: 단순한 동전 챙기기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251116/단순한 동전 챙기기/collect-coins-easy.cpp
+++ 251116/단순한 동전 챙기기/collect-coins-easy.cpp
@@ -1,61 +1,80 @@
 #include <iostream>
 #include <climits>
+#include <vector>
 using namespace std;
 
 int N;
-char grid[20][20];
+char grid[21][21];
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 int ansMin = INT_MAX;
+int pathMin = -1;
+int endX, endY;
 bool gFlag = false;
 
-void FindPath(int r, int c, int cnt, int coin, int coinCount)
+pair<int, int> numbers[10];
+
+int Find(int r1, int c1, int r2, int c2)
 {
-    if (cnt == N * N)
-    {
-        return;
-    }
-    if (grid[r][c] == 'E')
-    {
-        if (coinCount >= 3)
-        {
-            ansMin = min(ansMin, cnt);
-            gFlag = true;
-        }
-        return;
-    }
+	int maxX = max(r1, r2);
+	int maxY = max(c1, c2);
 
-    for (int d = 0; d < 4; ++d)
-    {
-        int x = r + dx[d];
-        int y = c + dy[d];
+	int minX = min(r1, r2);
+	int minY = min(c1, c2);
 
-        if (x < 0 || y < 0 || x >= N || y >= N) continue;
-        int num = grid[x][y] - '0';
-        if (num <= 9 && num >= 1 && coin < num) FindPath(x, y, cnt + 1, num, coinCount + 1);
-        else FindPath(x, y, cnt + 1, coin, coinCount);
-    }
+	return maxX - minX + maxY - minY;
+}
+
+void FindPath(int idx, int coin, int coinCount, int total, int r, int c)
+{
+	if (coinCount >= 3)
+	{
+		ansMin = min(ansMin, total + Find(numbers[coin].first, numbers[coin].second, endX, endY));
+		gFlag = true;
+		return;
+	}
+
+	for (int i = idx; i < 10; ++i)
+	{
+		int x = numbers[i].first;
+		int y = numbers[i].second;
+		if (x != 0 && y != 0)
+		{
+			FindPath(i + 1, i, coinCount + 1, total + Find(r, c, x, y), x, y);
+		}
+		FindPath(i + 1, coin, coinCount, total, r, c);
+	}
 }
 
 
 int main() {
-    cin >> N;
-    int r, c;
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-        {
-            cin >> grid[i][j];
-            if (grid[i][j] == 'S')
-            {
-                r = i;
-                c = j;
-            }
-        }
+	int r, c;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+		{
+			cin >> grid[i][j];
+			if (grid[i][j] == 'S')
+			{
+				r = i;
+				c = j;
+			}
+			if (grid[i][j] - '0' <= 9)
+			{
+				numbers[grid[i][j] - '0'] = (make_pair(i, j));
+			}
+			if (grid[i][j] == 'E')
+			{
+				endX = i;
+				endY = j;
+			}
+		}
 
-    // Please write your code here.
+	// Please write your code here.
 
-    FindPath(r, c, 0, -1, 0);
-    if (gFlag) cout << ansMin;
-    else cout << -1;
-    return 0;
+	FindPath(1, 0, 0, 0, r, c);
+
+	if (gFlag) cout << ansMin;
+	else cout << -1;
+	return 0;
 }
