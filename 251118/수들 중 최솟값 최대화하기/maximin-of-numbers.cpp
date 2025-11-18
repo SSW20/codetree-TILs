ccb8464e42c74f4b5439d239e1d010410edb6058
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-18 13:38:25 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 30ms, Memory: 0MB, Status: Passed - Codetree

--- 251118/수들 중 최솟값 최대화하기/maximin-of-numbers.cpp
+++ 251118/수들 중 최솟값 최대화하기/maximin-of-numbers.cpp
@@ -8,20 +8,33 @@ int grid[10][10];
 int visited[10];
 vector<int> v;
 int ansMax = -1;
+
+int Find()
+{
+    int temp = 1000000;
+    for (int x : v)
+    {
+        if (temp > x) temp = x;
+    }
+    return temp;
+}
+
 void Select(int row)
 {
     if (row == n)
     {
-        sort(v.begin(), v.end());
-        ansMax = max(ansMax, v[0]);
+        ansMax = max(ansMax, Find());
         return;
     }
     for (int i = 0; i < n; ++i)
     {
         if (visited[i] == 1) continue;
+
         visited[i] = 1;
         v.push_back(grid[row][i]);
+
         Select(row + 1);
+
         visited[i] = 0;
         v.pop_back();
 
