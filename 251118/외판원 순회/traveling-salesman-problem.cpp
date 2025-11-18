29e3114a6144cdc3065d5ad92ff84fe5137dd713
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-18 13:08:31 +0900

    Title: 외판원 순회, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251118/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n;
+int A[10][10];
+int ansMinX = INT_MAX;
+int visitedX[10];
+
+
+void CalcX(int cnt, int total, int lastCity)
+{
+    if (cnt == n)
+    {
+        total += A[lastCity][0];
+        ansMinX = min(ansMinX, total);
+        return;
+    }
+
+    for (int i = 0; i < n; ++i)
+    { 
+        if (visitedX[i] || A[lastCity][i] == 0) continue;
+        visitedX[i] = 1;
+        CalcX(cnt + 1, total + A[lastCity][i], i);
+        visitedX[i] = 0;
+    }
+
+    
+}
+
+
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    CalcX(0, 0, 0);
+    // Please write your code here.
+    cout << ansMinX;
+    return 0;
+}
