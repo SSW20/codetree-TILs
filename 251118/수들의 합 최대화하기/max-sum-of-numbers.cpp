239da5156dd917fd1c3c6627671234807a4961cf
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-18 12:33:18 +0900

    Title: 수들의 합 최대화하기, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251118/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[10][10];
+int visited[10];
+int ansMax = -1;
+void Select(int row, int total)
+{
+    if (row == n)
+    {
+        ansMax = max(ansMax, total);
+        return;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (visited[i] == 1) continue;
+        visited[i] = 1;
+        Select(row + 1, total + grid[row][i]);
+        visited[i] = 0;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    Select(0, 0);
+    cout << ansMax;
+    // Please write your code here.
+
+    return 0;
+}
