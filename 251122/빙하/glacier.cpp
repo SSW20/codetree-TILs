094d89ab77145e217160a8020ac6ba0c767e82af
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-22 10:45:39 +0900

    Title: 빙하, Time: 80ms, Memory: 1MB, Status: Passed - Codetree

--- 251122/빙하/glacier.cpp
+++ 251122/빙하/glacier.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <cstring>
 using namespace std;
 
 int N, M;
@@ -8,94 +9,6 @@ int temp_a[200][200];
 int visited[200][200];
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
-queue<pair<int, int>> q;
-bool check()
-{
-    for (int i = 0; i < N; ++i)
-    {
-        for (int k = 0; k < M; ++k)
-        {
-            if (a[i][k] == 1) return false;
-        }
-    }
-    return true;
-}
-
-void clear()
-{
-    for (int i = 0; i < N; ++i)
-    {
-        for (int k = 0; k < M; ++k)
-        {
-            a[i][k] = temp_a[i][k];
-        }
-    }
-}
-
-pair<int,int> Find()
-{
-    int minX = 300, minY = 300;
-    for (int i = 0; i < N; ++i)
-    {
-        for (int k = 0; k < M; ++k)
-        {
-            if (a[i][k] == 0 && visited[i][k] == 0)
-            {
-                for (int d = 0; d < 4; ++d)
-                {
-                    int x = i + dx[d];
-                    int y = k + dy[d];
-
-                    if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] == 1) continue;
-                    if (a[x][y] == 0)
-                    {
-                        minX = min(minX, x);
-                        minY = min(minY, y);
-                    }
-                }
-            }
-        }
-    }
-
-    return make_pair(minX, minY);
-}
-
-
-void BFS(int time, int count)
-{
-    if (check())
-    {
-        cout << time << ' ' << count;
-        return;
-    }
-
-    count = 0;
-    pair<int, int> start = Find();
-    q.push(start);
-    visited[start.first][start.second] = 1;
-    while (!q.empty())
-    {
-        int start_x = q.front().first;
-        int start_y = q.front().second;
-        
-        q.pop();
-
-        for (int d = 0; d < 4; ++d)
-        {
-            int x = start_x + dx[d];
-            int y = start_y + dy[d];
-
-            if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] == 1) continue;
-            if (a[x][y] == 1 && temp_a[x][y] == 1) { temp_a[x][y] = 0; count++;  continue; }
-            if (a[x][y] == 1) continue;
-            q.push(make_pair(x, y));
-            visited[x][y] = 1;
-        }
-    }
-
-    clear();
-    BFS(time + 1, count);
-}
 
 int main() {
     cin >> N >> M;
@@ -107,8 +20,49 @@ int main() {
         }
     }
 
-    BFS(0,0);
+    int time = 0;
+    int last_ice_count = 0;
+    
     // Please write your code here.
+    while (true)
+    {
+        memset(visited, 0, sizeof(visited));
+        queue<pair<int, int>> q;
+        q.push(make_pair(0, 0));
+        visited[0][0] = 1;
+        int current_ice_count = 0;
+        while (!q.empty())
+        {
+            int start_x = q.front().first;
+            int start_y = q.front().second;
+            q.pop();
 
+            for (int d = 0; d < 4; ++d)
+            {
+                int x = start_x + dx[d];
+                int y = start_y + dy[d];
+
+                if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] == 1) continue;
+                if (a[x][y] == 0)
+                {
+                    q.push(make_pair(x, y));
+                    visited[x][y] = 1;
+                }
+                else 
+                {
+                    visited[x][y] = 1;
+                    current_ice_count++;
+                    a[x][y] = 0;
+                }
+            }
+        }
+
+        if (current_ice_count == 0) break;
+
+        time++;
+        last_ice_count = current_ice_count;
+    }
+
+    cout << time << " " << last_ice_count;
     return 0;
 }
