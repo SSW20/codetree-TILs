9da13a4ab78423a52e864c1416613f89ee90e1e4
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-29 14:35:55 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 79ms, Memory: 2MB, Status: Wrong Answer - Codetree

--- 251129/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 251129/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -9,48 +9,48 @@ long long int ans[500][500];
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 
+int Travel(int cnt, int r, int c)
+{
+    int maxCnt = 0;
+    for (int d = 0; d < 4; ++d)
+    {
+        int x = r + dx[d];
+        int y = c + dy[d];
+        if (x < 0 || y < 0 || x >= n || y >= n) continue;
+        if (grid[x][y] <= grid[r][c] || ans[x][y] > ans[r][c]) continue;
+        maxCnt = max(maxCnt, cnt + Travel(cnt, x, y));
+        
+    }
+    ans[r][c] = maxCnt;
+    return maxCnt;
+}
+
 int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
-            ans[i][j] = 1;
         }
     }
 
     // Please write your code here.
 
-    stack<pair<int, int>> st;
     long long int ansMax = -1;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
         {
-            if (visited[i][j]) continue;
-            st.push({ i,j });
-            visited[i][j] = 1;
-            while (!st.empty())
-            {
-                int curX = st.top().first;
-                int curY = st.top().second;
-                st.pop();
-                for (int d = 0; d < 4; ++d)
-                {
-                    int x = curX + dx[d];
-                    int y = curY + dy[d];
-                    if (x < 0 || y < 0 || x >= n || y >= n) continue;
-                    if (grid[x][y] <= grid[curX][curY] || ans[x][y] > ans[curX][curY] + 1) continue;
-                    st.push({ x,y });
-                    ans[x][y] = ans[curX][curY] + 1;
-                    ansMax = max(ans[x][y], ansMax);
-                    visited[x][y] = 1;
-                }
-            }
+            if (ans[i][j] == 0) Travel(1, i, j);
         }
     }
-
-    cout << ansMax;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+        {
+            ansMax = max(ansMax, ans[i][j]);
+        }
+    }
+    cout << ansMax + 1;
 
 
     return 0;
