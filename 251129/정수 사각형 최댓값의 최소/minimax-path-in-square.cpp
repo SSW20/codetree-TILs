4436829f588a186a4ccd144b7661ae25e17c208c
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-11-29 15:30:30 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 11ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251129/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[100][100];
+int ans[100][100];
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    ans[0][0] = grid[0][0];
+
+
+    int rowMin = grid[0][0];
+    for (int i = 1; i < n; i++)
+    {
+        rowMin = max(rowMin, grid[0][i]);
+        ans[0][i] = rowMin;
+    }
+
+    int colMin = grid[0][0];
+    for (int i = 1; i < n; i++)
+    {
+        colMin = max(colMin, grid[i][0]);
+        ans[i][0] = colMin;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if(grid[i][j] < min(ans[i - 1][j], ans[i][j - 1]))
+            {
+                ans[i][j] = min(ans[i - 1][j], ans[i][j - 1]);
+            }
+            else
+            {
+               ans[i][j] = grid[i][j];
+            }
+        }
+    }
+    cout << ans[n - 1][n - 1];
+
+    return 0;
+}
