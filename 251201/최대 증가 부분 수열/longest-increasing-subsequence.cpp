e793d990ba17fee04614d7c880cfa859a7375a32
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-01 12:23:28 +0900

    Title: 최대 증가 부분 수열, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 251201/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 251201/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -9,8 +9,8 @@ int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> M[i];
+        dp[i] = 1;
     }
-    dp[0] = 1;
 
     for (int i = 1; i < N; i++) {
         for (int j = 0; j < i; ++j)
