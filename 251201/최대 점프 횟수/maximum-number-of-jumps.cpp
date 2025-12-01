c55d835126c2d599d80e09d8b6a3d0232983eb5c
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-01 12:43:01 +0900

    Title: 최대 점프 횟수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251201/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int M[1000];
+int dp[1000];
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> M[i];
+    }
+
+    dp[0] = 0;
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; ++j)
+        {
+            if (j + M[j] >= i)
+            {
+                if(j == 0)
+                    dp[i] = max(dp[i], dp[j] + 1);
+                else
+                {
+                    if(dp[j] == 0) continue;
+                    else dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+          
+        }
+    }
+
+    int ansMax = -1;
+    for (int i = 0; i < N; ++i)
+    {
+        ansMax = max(ansMax, dp[i]);
+    }
+
+    cout << ansMax;
+
+
+    return 0;
+}
