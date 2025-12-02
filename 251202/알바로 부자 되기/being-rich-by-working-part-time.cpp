288528499f066b258e38c9b07594aa1a16d628b2
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-02 12:08:07 +0900

    Title: 알바로 부자 되기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251202/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int s[1000], e[1000], p[1000];
+long long int dp[1000];
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> s[i] >> e[i] >> p[i];
+        dp[i] = p[i];
+    }
+
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; ++j)
+        {
+            if (s[i] > e[j])
+            {
+                dp[i] = max(dp[i], dp[j] + dp[i]);
+            }
+        }
+    }
+
+    long long int ansMax = -1;
+    for (int i = 0; i < N; i++)
+    {
+        ansMax = max(dp[i], ansMax);
+    }
+    cout << ansMax;
+
+
+    return 0;
+}
