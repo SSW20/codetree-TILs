b29c349a4fca82dca7b06fc5ab4e0bf7d75db282
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-11 11:10:04 +0900

    Title: 축구와 야구, Time: 19ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251211/축구와 야구/soccer-and-baseball.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int n;
+vector<pair<int, int>> v;
+long long dp[12][10];
+
+int main() {
+    cin >> n;
+    int s, b;
+    for (int i = 0; i < n; i++) {
+        cin >> s >> b;
+        v.push_back({ s,b });
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int k = 0; k < 10; ++k)
+        {
+            dp[i][k] = INT_MIN;
+        }
+    }
+
+    dp[0][0] = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int cur_socc = v[i].first;
+        int cur_base = v[i].second;
+
+        for (int j = 11; j > -1; --j)
+        {
+            for (int k = 9; k > -1; --k)
+            {
+                if (dp[i][k] == INT_MIN) continue;
+                
+                if (j < 11)
+                {
+                    dp[j + 1][k] = max(dp[j + 1][k], dp[j][k] + cur_socc);
+                }
+                if (k < 9)
+                {
+                    dp[j][k + 1] = max(dp[j][k + 1], dp[j][k] + cur_base);
+                }
+            }
+        }
+    }
+    cout << dp[11][9];
+    return 0;
+}
