8ddb96364ce071cf7967b19239a9341bfa584cad
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-14 13:42:58 +0900

    Title: 신전 탐험하기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251214/신전 탐험하기 2/explore-temple-2.cpp
+++ 251214/신전 탐험하기 2/explore-temple-2.cpp
@@ -28,22 +28,23 @@ int main() {
             dp[0][j][k] = 0;
         }
     }
-    
+
 
 
     for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= 3; ++j)
         {
-            for (int k = 1; k <= 3; ++k)
+            for (int t = 1; t <= 3; ++t)
             {
-                if (j != k)
+                for (int k = 1; k <= 3; ++k)
                 {
-                    for (int t = 1; t <= 3; ++t)
+                    if (j != k)
                     {
-                        if (dp[i - 1][j][t] == INT_MIN) continue;
-                        if (i == n && t != j) continue;
-                        dp[i][j][t] = max(dp[i][j][t], dp[i - 1][k][t] + map[i][j]);
+
+                            if (dp[i - 1][k][t] == INT_MIN) continue;
+                            if (i == n && t == j) continue;
+                            dp[i][j][t] = max(dp[i][j][t], dp[i - 1][k][t] + map[i][j]);
                     }
                 }
             }
@@ -54,7 +55,7 @@ int main() {
     for (int i = 0; i < 4; ++i)
     {
         for (int j = 1; j <= 3; ++j)
-         ansMax = max(ansMax, dp[n][i][j]);
+            ansMax = max(ansMax, dp[n][i][j]);
     }
 
     cout << ansMax;
