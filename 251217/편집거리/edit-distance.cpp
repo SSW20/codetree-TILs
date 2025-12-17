b80b63b9a24958bd12ae84bce3e01b6a9d79418f
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-17 12:34:35 +0900

    Title: 편집거리, Time: 14ms, Memory: 4MB, Status: Passed - Codetree

---
+++ 251217/편집거리/edit-distance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A, B;
+int dp[1001][1001];
+int main() {
+    cin >> A;
+    cin >> B;
+    
+    for (int i = 0; i <= A.length(); i++) dp[i][0] = i;
+    for (int j = 0; j <= B.length(); j++) dp[0][j] = j;
+
+    for (int i = 1; i <= A.length(); ++i)
+    {
+        for (int k = 1; k <= B.length(); ++k)
+        {
+            if (A[i-1] != B[k- 1])
+            {
+                dp[i][k] = min(min(dp[i - 1][k], dp[i][k - 1]), dp[i - 1][k - 1]) + 1;
+            }
+            else
+            {
+                dp[i][k] = dp[i - 1][k - 1];
+            }
+        }
+    }
+
+    cout << dp[A.length()][B.length()];
+    return 0;
+}
