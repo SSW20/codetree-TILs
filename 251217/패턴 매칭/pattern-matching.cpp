0cd21c81dc6fe66a07171242db7c8405b40216a3
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-17 13:24:38 +0900

    Title: 패턴 매칭, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251217/패턴 매칭/pattern-matching.cpp
+++ 251217/패턴 매칭/pattern-matching.cpp
@@ -1,10 +1,11 @@
     #include <iostream>
     #include <string>
-
+#include <set>
     using namespace std;
 
     string S, P;
     bool dp[1001][1001];
+    set<char> aster;
     int main() {
         cin >> S;
         cin >> P;
@@ -13,23 +14,24 @@
 
         for (int j = 2; j <= P.length(); j++) {
             if (P[j - 1] == '*') {
+                aster.insert(P[j - 2]);
                 dp[0][j] = dp[0][j - 2];
             }
         }
 
-        for (int i = 1; i <= S.length(); ++i)
+        for (int i = 1; i <= S.length(); ++i)   
         {
             for (int k = 1; k <= P.length(); ++k)
             {
                 if (S[i - 1] == P[k - 1] || P[k - 1] == '.')
                 {
-                    dp[i][k] = dp[i - 1][k - 1];
+                    dp[i][k] = dp[i-1][k-1];
                 }
                 else
                 {
                     if (P[k - 1] == '*')
                     {
-                        dp[i][k] = dp[i - 2][k];
+                        dp[i][k] = dp[i][k - 2];
                         if (S[i - 1] == P[k - 2] || P[k - 2] == '.')
                         {
                             dp[i][k] = dp[i][k] || dp[i - 1][k];
