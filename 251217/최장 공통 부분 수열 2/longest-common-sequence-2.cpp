c9a57b7259e68b33a2b098d4f8f52bcb9f18dbba
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-17 14:26:13 +0900

    Title: 최장 공통 부분 수열 2, Time: 14ms, Memory: 4MB, Status: Passed - Codetree

--- 251217/최장 공통 부분 수열 2/longest-common-sequence-2.cpp
+++ 251217/최장 공통 부분 수열 2/longest-common-sequence-2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
 string A, B;
-pair<int,string> dp[1001][1001];
+int dp[1001][1001];
 int main() {
     cin >> A;
     cin >> B;
@@ -14,23 +14,33 @@ int main() {
     {
         for (int k = 1; k <= B.length(); ++k)
         {
-            if(A[i - 1] == B[k - 1])
-            {
-                dp[i][k] = { dp[i - 1][k - 1].first + 1, dp[i - 1][k - 1].second + A[i - 1] };
-            }
-            else
-            {
-                if (dp[i - 1][k].first > dp[i][k - 1].first)
-                {
-                    dp[i][k] = { dp[i - 1][k].first, dp[i - 1][k].second };
-                }
-                else
-                {
-                    dp[i][k] = { dp[i][k - 1].first, dp[i][k - 1].second };
-                }
-            }
+            dp[i][k] = (A[i - 1] == B[k - 1] ? dp[i - 1][k - 1] + 1 : max(dp[i - 1][k], dp[i][k - 1]));
         }
     }
-    cout << dp[A.length()][B.length()].second;
+    int x = A.length();
+    int y = B.length();
+
+    string ans = "";
+
+    while (x > 0 && y > 0)
+    {
+        if (A[x - 1] == B[y - 1])
+        {
+            ans += A[x - 1];
+            --x;
+            --y;
+        }
+        else if (dp[x][y] == dp[x][y-1])
+        {
+            y--;
+        }
+        else
+        {
+            x--;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans;
     return 0;
 }
