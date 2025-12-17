439aa090942c0986e621f447386a97e3d5a4cfaa
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-17 13:12:32 +0900

    Title: 패턴 매칭, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251217/패턴 매칭/pattern-matching.cpp
@@ -0,0 +1,45 @@
+    #include <iostream>
+    #include <string>
+
+    using namespace std;
+
+    string S, P;
+    bool dp[1001][1001];
+    int main() {
+        cin >> S;
+        cin >> P;
+
+        dp[0][0] = true;
+
+        for (int j = 2; j <= P.length(); j++) {
+            if (P[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= S.length(); ++i)
+        {
+            for (int k = 1; k <= P.length(); ++k)
+            {
+                if (S[i - 1] == P[k - 1] || P[k - 1] == '.')
+                {
+                    dp[i][k] = dp[i - 1][k - 1];
+                }
+                else
+                {
+                    if (P[k - 1] == '*')
+                    {
+                        dp[i][k] = dp[i - 2][k];
+                        if (S[i - 1] == P[k - 2] || P[k - 2] == '.')
+                        {
+                            dp[i][k] = dp[i][k] || dp[i - 1][k];
+                        }
+                    }
+                }
+            }
+        }
+
+        if (dp[S.length()][P.length()]) cout << "true";
+        else cout << "false";
+        return 0;
+    }
