b8f7e6ebf129bb2350a87bd375aaee7b5e205ed2
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-17 14:14:45 +0900

    Title: 최장 공통 부분 수열 2, Time: 255ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 251217/최장 공통 부분 수열 2/longest-common-sequence-2.cpp
+++ 251217/최장 공통 부분 수열 2/longest-common-sequence-2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 string A, B;
-int dp[1001][1001];
+pair<int,string> dp[1001][1001];
 int main() {
     cin >> A;
     cin >> B;
@@ -14,12 +14,23 @@ int main() {
     {
         for (int k = 1; k <= B.length(); ++k)
         {
-            dp[i][k] = (A[i - 1] == B[k - 1] ? dp[i - 1][k - 1] + 1 : max(dp[i - 1][k], dp[i][k - 1]));
+            if(A[i - 1] == B[k - 1])
+            {
+                dp[i][k] = { dp[i - 1][k - 1].first + 1, dp[i - 1][k - 1].second + A[i - 1] };
+            }
+            else
+            {
+                if (dp[i - 1][k].first > dp[i][k - 1].first)
+                {
+                    dp[i][k] = { dp[i - 1][k].first, dp[i - 1][k].second };
+                }
+                else
+                {
+                    dp[i][k] = { dp[i][k - 1].first, dp[i][k - 1].second };
+                }
+            }
         }
     }
-    for (int i = 1; i <= A.length(); ++i)
-    {
-        if(dp[i][B.length()] != dp[i - 1][B.length()]) cout << A[i - 1];
-    }
+    cout << dp[A.length()][B.length()].second;
     return 0;
 }
