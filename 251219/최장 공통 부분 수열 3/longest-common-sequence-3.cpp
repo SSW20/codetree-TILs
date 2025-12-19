935e8fc6cc4459df73498c086aa8717b64a0c7e6
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-19 10:27:30 +0900

    Title: 최장 공통 부분 수열 3, Time: 12ms, Memory: 6MB, Status: Wrong Answer - Codetree

--- 251219/최장 공통 부분 수열 3/longest-common-sequence-3.cpp
+++ 251219/최장 공통 부분 수열 3/longest-common-sequence-3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int n, m;
 int a[1001];
 int b[1001];
-int dp[1001][1001];
+pair<int,int> dp[1001][1001];
 int main() {
     cin >> n >> m;
 
@@ -17,21 +17,37 @@ int main() {
     for (int i = 1; i <= m; i++) {
         cin >> b[i];
     }
-
+    vector<int> an;
     for (int i = 1; i <= n; ++i)
     {
         for (int k = 1; k <= m; ++k)
         {
-            if (a[i] == b[k]) dp[i][k] = dp[i - 1][k - 1] + 1;
-            else dp[i][k] = max(dp[i - 1][k], dp[i][k - 1]);
+            if (a[i] == b[k])
+            {
+                dp[i][k].first = dp[i - 1][k - 1].first + 1;
+                dp[i][k].second = min(a[i],b[k]);
+            }
+            else
+            {
+                if (dp[i - 1][k].first > dp[i][k - 1].first)
+                {
+                    dp[i][k].first = dp[i - 1][k].first;
+                    dp[i][k].second = dp[i - 1][k].second;
+                }
+                else
+                {
+                    dp[i][k].first = dp[i][k - 1].first;
+                    dp[i][k].second = dp[i][k - 1].second;
+                }
+            }
         }
     }
 
     int x = n;
     int y = m;
-    int idx = dp[x][y];
-    vector<int> ans(dp[x][y]);
-    while (x > 0 && y > 0 && idx > -1)
+    int idx = dp[x][y].first;
+    vector<int> ans(idx);
+    while (x > 0 && y > 0)
     {
         if (a[x] == b[y])
         {
@@ -41,14 +57,14 @@ int main() {
         }
         else
         {
-            if (dp[x][y] == dp[x][y-1] && dp[x][y] == dp[x-1][y])
+            if (dp[x][y].first == dp[x][y-1].first && dp[x][y].first == dp[x-1][y].first)
             {
                 if (a[x] < b[y]) y--;
                 else x--;
             }
-            else if (dp[x][y] == dp[x][y - 1] || dp[x][y] == dp[x - 1][y])
+            else if (dp[x][y].first == dp[x][y - 1].first || dp[x][y].first == dp[x - 1][y].first)
             {
-                if (dp[x][y] == dp[x][y - 1]) y--;
+                if (dp[x][y].first == dp[x][y - 1].first) y--;
                 else x--;
             }
         }
