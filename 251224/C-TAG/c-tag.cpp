a36b0a24f69937d619ef2cc8dc4350efd1e63aa5
SSW20 <76836826+SSW20@users.noreply.github.com>
2025-12-24 11:43:07 +0900

    Title: C-TAG, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251224/C-TAG/c-tag.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+int n, m;
+string A[500];
+string B[500];
+
+unordered_set<string> uset;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) cin >> A[i];
+
+	for (int i = 0; i < n; i++) cin >> B[i];
+
+	int ans = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = i + 1; j < m; j++)
+			for (int k = j + 1; k < m; k++)
+			{
+				for (int t = 0; t < n; ++t)
+				{
+					string temp = "";
+					temp += A[t][i] + A[t][j] + A[t][k];
+					uset.insert(temp);
+				}
+
+				bool bCan = true;
+
+				for (int t = 0; t < n; ++t)
+				{
+					string temp = "";
+					temp += B[t][i] + B[t][j] + B[t][k];
+					
+					if (uset.find(temp) != uset.end())
+					{
+						bCan = false;
+						break;
+					}
+				}
+
+				if (bCan) ++ans;
+				uset.clear();
+			}
+
+	cout << ans;
+	return 0;
+}
